aef7bf2251f809a042cd192119061d8f72e2d75c
kamilFitrzyk <kamilfitrzyk89@gmail.com>
2023-01-22 02:11:13 +0100

    Problem 17

--- src/edu/euler/Euler17.java
+++ src/edu/euler/Euler17.java
@@ -5,45 +5,75 @@ import java.util.Map;
 
 public class Euler17 {
 
-    public static void main(String[] args) {
-        Map<Integer, Integer> number = new HashMap<>();
 
-        number.put(1, 3);
-        number.put(2, 3);
-        number.put(3, 3);
-        number.put(4, 3);
-        number.put(5, 3);
-        number.put(6, 3);
-        number.put(7, 3);
-        number.put(8, 3);
-        number.put(9, 3);
-        number.put(10, 3);
-        number.put(11, 3);
-        number.put(12, 3);
-        number.put(13, 3);
-        number.put(14, 3);
-        number.put(15, 3);
-        number.put(16, 3);
-        number.put(17, 3);
-        number.put(18, 3);
-        number.put(19, 3);
-        number.put(20, 3);
-        number.put(30, 3);
-        number.put(40, 3);
-        number.put(50, 3);
-        number.put(60, 3);
-        number.put(70, 3);
-        number.put(80, 3);
-        number.put(90, 3);
-        number.put(100, 3);
-        number.put(1000, 3);
+    public static void main(String[] args) {
+        Map<Integer, Integer> numberCounterMap = new HashMap<>();
+
+        numberCounterMap.put(0, 0);
+        numberCounterMap.put(1, 3);
+        numberCounterMap.put(2, 3);
+        numberCounterMap.put(3, 5);
+        numberCounterMap.put(4, 4);
+        numberCounterMap.put(5, 4);
+        numberCounterMap.put(6, 3);
+        numberCounterMap.put(7, 5);
+        numberCounterMap.put(8, 5);
+        numberCounterMap.put(9, 4);
+        numberCounterMap.put(10, 3);
+        numberCounterMap.put(11, 6);
+        numberCounterMap.put(12, 6);
+        numberCounterMap.put(13, 8);
+        numberCounterMap.put(14, 8);
+        numberCounterMap.put(15, 7);
+        numberCounterMap.put(16, 7);
+        numberCounterMap.put(17, 9);
+        numberCounterMap.put(18, 8);
+        numberCounterMap.put(19, 8);
+        numberCounterMap.put(20, 6);
+        numberCounterMap.put(30, 6);
+        numberCounterMap.put(40, 5);
+        numberCounterMap.put(50, 5);
+        numberCounterMap.put(60, 5);
+        numberCounterMap.put(70, 7);
+        numberCounterMap.put(80, 6);
+        numberCounterMap.put(90, 6);
+        numberCounterMap.put(100, 7);
+        numberCounterMap.put(1000, 5);
 
         int counter = 0;
 
-        for (int i = 1; i <= 1000; i++) {
+        for (int number = 1; number <= 1000; number++) {
+            if (number == 1000) {
+                counter += numberCounterMap.get(1);
+                counter += numberCounterMap.get(1000);
+                counter += 3;
+            } else {
 
+                int restOfTon = number % 100;
+                int tons = (number - restOfTon) / 100;
+
+                int restOfDozens = restOfTon % 10;
+                int dozens = (restOfTon - restOfDozens) / 10;
+
+                if (tons >= 1) {
+                    counter += numberCounterMap.get(tons);
+                    counter += numberCounterMap.get(100);
+                }
+                if (tons >= 1 && restOfTon != 0) {
+                    counter += 3;
+                }
+                if (restOfTon <= 19) {
+                    counter += numberCounterMap.get(restOfTon);
+                } else {
+                    counter += numberCounterMap.get(restOfDozens);
+                    counter += numberCounterMap.get(dozens * 10);
+                }
+
+            }
         }
-
+        System.out.println(counter);
 
     }
+
+
 }
